bfc49b78956192677e1c06b58125b5f3b72cd189
nelimalu <lukaj2501@gmail.com>
2025-11-20 00:28:56 -0500

    added message hash signature

---
+++ rsa-test/sha256.py
@@ -0,0 +1,11 @@
+import hashlib
+
+
+def hash(message: str) -> str:
+    sha256_hash = hashlib.sha256()
+    sha256_hash.update(message.encode('utf-8'))
+    return sha256_hash.hexdigest()
+
+
+if __name__ == "__main__":
+    print(hash("hello"))
